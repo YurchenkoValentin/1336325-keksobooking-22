865d05e6aefc5debf367268919d91a2ffa0b1671
Valentin <yurchenkovalentin.dev@gmail.com>
2021-02-08 11:58:29 +0300

    Улучшает читаемость кода

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 /*jslint node: true */
 'use strict';
 
-// Изменил способ объявления функций на стрелочный. Но стрелочный мне
-// меньше нравится, т.к. много ньюансов в более сложных функциях.
 const getRandomNumber = (min, max) => {
   if (max <= min) {
     alert('ошибка! Минимальное число должно быть меньше максимального');
@@ -13,16 +11,19 @@ const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-(getRandomNumber(20, 70));
+getRandomNumber(20, 70);
 
-// Оставил ключевое слово let, потому что "...Будет использоваться для
-// генерации временных географических координат в следующем задании."
-let getCoordinates = (min, max, numberOfSymbols) => {
+
+const getCoordinates = (min, max, numberOfSymbols) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   let random = Math.random() * (max - min + 1) + min;
-
   return random.toFixed(numberOfSymbols);
+
 };
 
-(getCoordinates(1, 100, 3));
+getCoordinates(1, 100, 3);
+
+
+
+
